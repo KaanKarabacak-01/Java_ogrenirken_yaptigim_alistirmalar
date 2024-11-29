a60360711d8835412a62612ab4edacc47e7e2ebf
KaanKarabacak-01 <bilsemkaan@gmail.com>
2024-11-29 21:07:16 +0300

    Sayı piramidi
    Bu kod birden başlayıp girdiğiniz sayıya kadar olan bir sayı piramidi yapar.

---
+++ sayiPiramidi.java
@@ -0,0 +1,21 @@
+package calismalar;
+import java.util.Scanner;
+public class sayiPiramidi {
+//12
+	public static void main(String[] args) {
+		Scanner oku=new Scanner(System.in);
+		int sayi=oku.nextInt();
+		oku.close();
+		int bosluk=(sayi-1)*2;
+			for(int a=1;a<=sayi;a++) {
+				for(int d=0;d<=bosluk;d++) {System.out.print(" ");}
+				bosluk-=2;
+				for(int b=a;b>=1;b--) {System.out.print(b+" ");}
+				for(int c=2;c<=a;c++){System.out.print(c+" ");}
+				System.out.println();
+				
+			}
+
+	}
+
+}
