36bd18bf42e4696e06e21bddfd459b0ebb03bc15
KaanKarabacak-01 <bilsemkaan@gmail.com>
2024-11-29 21:05:49 +0300

    Eşkenar dörtgen
    Bu kod girdiğiniz sayının büyüklüğüne göre bir eşkenar üçgen çizmekte.

---
+++ baklavaDilimi.java
@@ -0,0 +1,47 @@
+package calismalar;
+import java.util.Scanner;
+public class baklavaDilimi {
+	public static void main(String[] args) {
+    Scanner oku=new Scanner(System.in);
+    int sayi=oku.nextInt();
+    oku.close();
+    int bosluk=sayi*2;
+    int n,b=0,i=0;
+    for(n=0;n<=sayi;n+=2)
+    {
+    	for(b=0;b<bosluk;b+=2)
+    	{
+    		System.out.print(" ");	
+    	}
+
+    	for(i=0;i<=n;i++)
+    	{
+    		System.out.print("*");
+    		
+    	}
+    	bosluk-=2;
+ 
+	System.out.println();
+
+    }
+    bosluk-=2;
+    for(n=n;n>=0;n-=2)
+    {
+    	for(int a=0;a<=bosluk;a+=2)
+    	{
+    		System.out.print(" ");	
+    	}
+    	bosluk+=2;
+    	for(int c=0;c<=n;c++)
+    	{
+    		System.out.print("*");
+
+    	}
+	System.out.println("");
+    }
+    
+    
+    
+	}
+
+}
