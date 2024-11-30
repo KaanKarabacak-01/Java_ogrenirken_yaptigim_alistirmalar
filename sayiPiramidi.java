6684bf99ccd00672ca55d3c15a51de486018b6d5
KaanKarabacak-01 <bilsemkaan@gmail.com>
2024-11-30 09:54:02 +0300

    Update sayiPiramidi.java

--- sayiPiramidi.java
+++ sayiPiramidi.java
@@ -4,6 +4,7 @@ public class sayiPiramidi {
 //12
 	public static void main(String[] args) {
 		Scanner oku=new Scanner(System.in);
+		System.out.println("Lütfen sayı giriniz: ");
 		int sayi=oku.nextInt();
 		oku.close();
 		int bosluk=(sayi-1)*2;
